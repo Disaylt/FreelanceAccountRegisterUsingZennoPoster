a03343cb3f6c7e26944654956ca8dfa6f0844cdd
disaylt <disofban@gmail.com>
2023-01-01 16:11:49 +0300

    feat: create factory proxyLoader

---
+++ ProxyLayer/Factory/TxtProxyLoaderFactory.cs
@@ -0,0 +1,27 @@
+﻿using ProxyLayer.Abstract;
+using ProxyLayer.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyLayer.Factory
+{
+    public class TxtProxyLoaderFactory : IProxyLoaderFactory
+    {
+        private readonly string _fileName;
+        
+        public TxtProxyLoaderFactory(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public IProxyLoader Create()
+        {
+            IProxyLoader proxyLoader = new TxtProxyLoader(_fileName);
+
+            return proxyLoader;
+        }
+    }
+}
