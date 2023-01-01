b797fdf3e3f83d98be61aa3e7433b2ef7d43c720
disaylt <disofban@gmail.com>
2023-01-01 21:03:18 +0300

    feat: add online sim manager

---
+++ PhoneNumbersLayer/Implements/OnlineSimPhoneNumberManager.cs
@@ -0,0 +1,27 @@
+﻿using PhoneNumbersLayer.Abstarct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumbersLayer.Implements
+{
+    internal class OnlineSimPhoneNumberManager : IPhoneNumberManager
+    {
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
