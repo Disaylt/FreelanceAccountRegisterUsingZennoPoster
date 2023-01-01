a847bbf18c55820bc277a7a63677387e27d69617
disaylt <disofban@gmail.com>
2023-01-01 13:51:22 +0300

    feat: create register manager

---
+++ AccountRegisterApplication/RegisterManager/Implementations/WbRegisterManager.cs
@@ -0,0 +1,17 @@
+﻿using AccountRegisterApplication.RegisterManager.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountRegisterApplication.RegisterManager.Implementations
+{
+    internal class WbRegisterManager : IRegisterManager
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
