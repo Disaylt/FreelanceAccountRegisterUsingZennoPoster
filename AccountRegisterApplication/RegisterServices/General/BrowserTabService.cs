cce8c1748b257b4513e0d966d48890603875621c
disaylt <disofban@gmail.com>
2023-01-03 20:22:48 +0300

    feat: add method for waiting element

---
+++ AccountRegisterApplication/RegisterServices/General/BrowserTabService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZennoLab.CommandCenter;
+
+namespace AccountRegisterApplication.RegisterServices.General
+{
+    internal class BrowserTabService
+    {
+        private readonly Instance _instance;
+
+        public BrowserTabService(Instance instance)
+        {
+            _instance = instance;
+        }
+
+        public bool WaitElement(string xpath, int waitTimeInSecond)
+        {
+            for(int i = 0; i < waitTimeInSecond; i++)
+            {
+                HtmlElement htmlElement = _instance.ActiveTab.GetDocumentByAddress("0").FindElementByXPath(xpath, 0);
+                if(!htmlElement.IsNull && !htmlElement.IsVoid)
+                {
+                    return true;
+                }
+                Thread.Sleep(1000);
+            }
+
+            return false;
+        }
+    }
+}
