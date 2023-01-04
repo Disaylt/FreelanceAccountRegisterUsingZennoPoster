3adf5400ca7f8b72eb1af36ec1dd238585a1f606
disaylt <disofban@gmail.com>
2023-01-04 12:24:27 +0300

    feat: create abstract captcha service

---
+++ CaptchaLayer/Abstract/ICaptchaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptchaLayer.Abstract
+{
+    public interface ICaptchaService<in T, out ResultT>
+    {
+        void Send(T captchaData);
+        ResultT Get();
+    }
+}
