7a2ad5b490296bf715fac0699b669fde7cd89aae
disaylt <disofban@gmail.com>
2023-01-05 13:35:36 +0300

    feat: create instance http manager

---
+++ AccountRegisterApplication/RegisterServices/General/InstanceHttpManager.cs
@@ -0,0 +1,28 @@
+﻿using HttpManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoLab.CommandCenter;
+
+namespace AccountRegisterApplication.RegisterServices.General
+{
+    internal abstract class InstanceHttpManager
+    {
+        private Instance _instance;
+        public InstanceHttpManager(Instance instance)
+        {
+            _instance = instance;
+        }
+
+        public IHttpSender HttpSender { get; private set; }
+
+        public void UpdateCookies()
+        {
+
+        }
+
+        private 
+    }
+}
