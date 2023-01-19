1e9df08fc2afc00814697327441ece815760e575
disaylt <disofban@gmail.com>
2023-01-20 00:35:21 +0300

    feat: create soax proxy settings loader and build factory

---
+++ ProxyLayer/Implementations/SoaxProxyLoader.cs
@@ -0,0 +1,29 @@
+﻿using ProxyLayer.Abstract;
+using ProxyLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyLayer.Implementations
+{
+    internal class SoaxProxyLoader : IProxyLoader
+    {
+        private readonly SoaxProxySettingsModel _settings;
+        private readonly static Random _random = new Random();
+
+        public SoaxProxyLoader(SoaxProxySettingsModel soaxProxySettingsModel)
+        {
+            _settings = soaxProxySettingsModel;
+        }
+
+        public string Get()
+        {
+            int port = _random.Next(_settings.MinPort, _settings.MaxProt);
+            string proxy = $"http://{_settings.Login}:{_settings.Password}@{_settings.Host}:{port}";
+
+            return proxy;
+        }
+    }
+}
